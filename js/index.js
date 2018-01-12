28727e929294036c6b45008c4dd8caac4ac3008d
Bernard Niset <bernard.niset@gmail.com>
2018-01-12 18:22:29 +0100

    Initial version in VCS.

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(3);
+}
+
+function draw() {
+  background(156);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
